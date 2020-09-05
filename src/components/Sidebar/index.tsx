0bc49b944935d38c981ef3c393a56c2b53f248de
unknown <viniciusbeloti7@gmail.com>
2020-09-05 16:23:29 -0300

    feat: add footer

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import './styles.css';
 
 const Sidebar: React.FC = () => {
-  return <div />;
+  return (
+    <div className="sidebar">
+      
+    </div>
+  );
 }
 
 export default Sidebar;
