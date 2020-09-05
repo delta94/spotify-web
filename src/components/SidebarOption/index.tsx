3b44c917046671185408e1c33c267df2869493fc
unknown <viniciusbeloti7@gmail.com>
2020-09-05 16:42:23 -0300

    chore: update Sidebar import SidebarOption

--- src/components/SidebarOption/index.tsx
+++ src/components/SidebarOption/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
-const SidebarOption: React.FC = () => {
-  return (
-      <div className="sidebaroption">
-          
-      </div>
-  );
+interface ISidebarOption {
+  title?: string;
+  Icon?: any;
+  option?: string;
 }
+
+const SidebarOption: React.FC<ISidebarOption> = ({ title, Icon }) => {
+  return (
+    <div className="sidebarOption">
+      <p>{title}</p>
+    </div>
+  );
+};
 
 export default SidebarOption;
