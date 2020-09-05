ed52029474a96f89552bcbf231a035f1d78120de
unknown <viniciusbeloti7@gmail.com>
2020-09-05 18:35:03 -0300

    feat: add SongRow styles

--- src/components/SongRow/index.tsx
+++ src/components/SongRow/index.tsx
@@ -3,11 +3,33 @@ import React from "react";
 import "./styles.css";
 
 interface ISongRow {
-  track: string;
+  track: {
+    name: string;
+    album: {
+      name: string;
+      images: {
+        url: string;
+      }[];
+    };
+    artists: {
+      name: string;
+    }[];
+  };
 }
 
 const SongRow: React.FC<ISongRow> = ({ track }) => {
-  return <div />;
+  return (
+    <div className="songRow">
+      <img className="songRow__album" src={track.album.images[0].url} alt={track.name} />
+      <div className="songRow__info">
+        <h1>{track.name}</h1>
+        <p>
+          {track.artists.map((artist) => artist.name).join(", ")} -{" "}
+          {track.album.name}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SongRow;
