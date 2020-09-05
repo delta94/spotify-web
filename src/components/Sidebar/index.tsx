300a87e49b895491287fee67e5dc3aa3baa90b1c
unknown <viniciusbeloti7@gmail.com>
2020-09-05 16:33:34 -0300

    feat: add Sidebaroption

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../assets/img/logo.svg';
 
 import './styles.css';
 
 const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
-      
+      <img className="sidebar__logo" src={logo} alt="Spotify Logo"/>
     </div>
   );
 }
---
+++ src/components/SidebarOption/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const SidebarOption: React.FC = () => {
+  return (
+      <div className="sidebaroption">
+          
+      </div>
+  );
+}
+
+export default SidebarOption;
